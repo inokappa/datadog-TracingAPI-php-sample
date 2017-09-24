78cc2fe333f466d7c719e35ff105771622db847a
inokappa <inokara@gmail.com>
2017-09-24 22:49:16 +0900

    update

--- sample1.php
+++ sample1.php
@@ -11,13 +11,12 @@ function ddtrace() {
     return $trace;
 }
 
-
 function durationCalculation($startTime) {
     return round(microtime(true), 4) - $startTime;
     return $durationTime;
 }
 
-function _1_sleep($trace, $parent_span_id) {
+function step_1_sleep($trace, $parent_span_id) {
     $startTime = round(microtime(true), 4);
 
     $rand_sleep_time = rand(1000000,2000000);
@@ -25,11 +24,11 @@ function _1_sleep($trace, $parent_span_id) {
 
     $span_id = rand();
     $durationTime = durationCalculation($startTime);
-    $span = $trace->generateSpandata('two_second_sleep', $span_id, $parent_span_id, $startTime, $durationTime);
+    $span = $trace->generateSpandata('step_one_sleep', $span_id, $parent_span_id, $startTime, $durationTime);
     return $span;
 }
 
-function _2_sleep($trace, $parent_span_id) {
+function step_2_sleep($trace, $parent_span_id) {
     $startTime = round(microtime(true), 4);
 
     $rand_sleep_time = rand(1500000,3000000);
@@ -37,7 +36,7 @@ function _2_sleep($trace, $parent_span_id) {
 
     $span_id = rand();
     $durationTime = durationCalculation($startTime);
-    $span = $trace->generateSpandata('one_second_sleep', $span_id, $parent_span_id, $startTime, $durationTime);
+    $span = $trace->generateSpandata('step_two_sleep', $span_id, $parent_span_id, $startTime, $durationTime);
     return $span;
 }
 
@@ -48,10 +47,10 @@ function root() {
    $span_id = rand();
    $spans = [];
 
-   $_1_span = _1_sleep($trace, $span_id);
-   array_push($spans, $_1_span);
-   $_2_span = _2_sleep($trace, $span_id);
-   array_push($spans, $_2_span);
+   $step_1_span = step_1_sleep($trace, $span_id);
+   array_push($spans, $step_1_span);
+   $step_2_span = step_2_sleep($trace, $span_id);
+   array_push($spans, $step_2_span);
 
    $durationTime = durationCalculation($startTime);
    $root_span = $trace->generateSpandata('root_sleep', $span_id, $parent_span_id = NULL, $startTime, $durationTime);
